0fa8b572e20a8f500100221c3c2d879d3b5ee43d
callmeSlikker <131174425+callmeSlikker@users.noreply.github.com>
2025-10-22 14:21:32 +0700

    fix bug SendCommandPanel

--- frontend/src/components/SendCommandPanel.jsx
+++ frontend/src/components/SendCommandPanel.jsx
@@ -1,42 +1,50 @@
 import { MsgUtils } from "../utils/MsgUtils";
 
+export default function SendCommandPanel({
+  command,
+  setCommand,
+  selectedPort,
+  selectedBaudrate,
+  appendLog,
+  selectedCommandName,
+}) {
 
-const sendCommand = async (hexCommand) => {
-  if (!hexCommand) {
-    appendLog({ text: "Please enter a command before sending.", commandName: selectedCommandName });
-    return;
-  }
-
-  try {
-    const res = await fetch("http://localhost:5000/sendCommand", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        port: selectedPort,
-        baudrate: parseInt(selectedBaudrate),
-        command: hexCommand,
-        name: selectedCommandName,
-      }),
-    });
-
-    const data = await res.json();
-
-    console.log('data', data);
-
-    if (data.log) {
-      data.log.split("\n").forEach(line => {
-        if (line.trim()) {
-          appendLog({ text: line, commandName: selectedCommandName });
-        }
-      });
-    } else {
-      appendLog({ text: "No log returned", commandName: selectedCommandName });
+  const sendCommand = async (hexCommand) => {
+    if (!hexCommand) {
+      appendLog({ text: "Please enter a command before sending.", commandName: selectedCommandName });
+      return;
     }
 
-  } catch (err) {
-    appendLog({ text: "Send command error: " + err.message, commandName: selectedCommandName });
-  }
+    try {
+      const res = await fetch("http://localhost:5000/sendCommand", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          port: selectedPort,
+          baudrate: parseInt(selectedBaudrate),
+          command: hexCommand,
+          name: selectedCommandName,
+        }),
+      });
 
+      const data = await res.json();
+
+      console.log('data', data);
+
+      if (data.log) {
+        data.log.split("\n").forEach(line => {
+          if (line.trim()) {
+            appendLog({ text: line, commandName: selectedCommandName });
+          }
+        });
+      } else {
+        appendLog({ text: "No log returned", commandName: selectedCommandName });
+      }
+
+    } catch (err) {
+      appendLog({ text: "Send command error: " + err.message, commandName: selectedCommandName });
+    }
+  };
 
   return (
     <div>
@@ -63,7 +71,6 @@ const sendCommand = async (hexCommand) => {
           onChange={(e) => setCommand(e.target.value)}
           placeholder="02 00 35 36 30 30 30 30 30 30 30 30 30 31 30 35 36 30 30 30 1C 34 30 00 12 30 30 30 30 30 30 30 30 30 31 30 30 1C 03 15"
           style={{
-
             height: "100%",
             width: "100%",
             backgroundColor: "#fff",
